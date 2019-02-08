8d7fcdb9efb9b246c8daa7d95ccb66e99c231e4a
obennisfeddoul <obennisfeddoul@sqli.com>
2019-02-08 11:53:31 +0100

    all test passed

--- src/main/java/com/indev/blackfriday/Company.java
+++ src/main/java/com/indev/blackfriday/Company.java
@@ -5,14 +5,30 @@ import java.util.HashMap;
 
 public class Company {
 
+    private  double MARGE_OF_BENEFITS = 1.2;
+    private   int QUANTITY_SELLED = 5;
+
     private HashMap<String,Product> products = new HashMap<String, Product>();
+    private HashMap<String,Product> productsSalled = new HashMap<String, Product>();
 
     public float sells(String capsule) {
-        return 0;
+        float newPrice = (float) (products.get(capsule).getPrix()*MARGE_OF_BENEFITS);
+
+        if(products.get(capsule).getQuantity()-QUANTITY_SELLED <0){
+            throw new RuntimeException();
+        }else{
+            products.get(capsule).setQuantity(products.get(capsule).getQuantity()-QUANTITY_SELLED);
+            if(productsSalled.containsKey(capsule)){
+                productsSalled.get(capsule).setQuantity(productsSalled.get(capsule).getQuantity()+QUANTITY_SELLED);
+            }else {
+                productsSalled.put(capsule,new Product(newPrice,capsule,QUANTITY_SELLED));
+            }
+        }
+        return newPrice*QUANTITY_SELLED;
     }
 
-    public void stock(int i, String capsule, int i1) {
-        products.put(capsule,new Product(i,capsule,i1));
+    public void stock(int quantity, String capsule, int price) {
+        products.put(capsule,new Product(price,capsule,quantity));
     }
 
     public Company to(int i) {
@@ -28,10 +44,23 @@ public class Company {
         for (Product p: products.values()) {
             total+=p.getPrix()*p.getQuantity();
         }
+        for (Product p: productsSalled.values()) {
+            total+=p.getPrix()*p.getQuantity();
+        }
         return (int) total;
     }
 
     public Company blackFriday() {
-        return this;
+        return new Company(1.1,QUANTITY_SELLED*2,products,productsSalled);
+    }
+
+    public Company(double MARGE_OF_BENEFITS, int QUANTITY_SELLED, HashMap<String, Product> products, HashMap<String, Product> productsSalled) {
+        this.MARGE_OF_BENEFITS = MARGE_OF_BENEFITS;
+        this.QUANTITY_SELLED = QUANTITY_SELLED;
+        this.products = products;
+        this.productsSalled = productsSalled;
+    }
+
+    public Company() {
     }
 }
